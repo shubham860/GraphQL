001ae65a27b4304e456ab8dbb635bbbe9f8e357a
shubham860 <shubham.chauhan860@gmail.com>
2021-04-18 01:57:27 +0530

    update

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,17 @@
-console.log('hello graphql');
+import { GraphQLServer } from 'graphql-yoga';
+
+const typeDefs = `
+    type Query {
+        intro : String!
+    }
+`;
+
+const resolvers = {
+    Query:  {
+        intro: () => 'Hello my self shubham'
+    }
+};
+
+
+const server = new GraphQLServer({typeDefs, resolvers});
+server.start(() => console.log('running on 4000'));
