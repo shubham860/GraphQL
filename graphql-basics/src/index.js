898b3759565236b87059e439d0c1bccb44008cfe
shubham860 <shubham.chauhan860@gmail.com>
2021-05-01 00:21:02 +0530

    custom types and operational arguments in resolvers

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,14 +1,42 @@
 import { GraphQLServer } from 'graphql-yoga';
 
+
+// typeDefs (Schema)
 const typeDefs = `
     type Query {
-        intro : String!
+       add(a: Float, b: Float): Float!,
+       greeting(name: String): String!,
+       me: User!,
+       post: Post!
+    }
+
+    type User {
+        id : ID!,
+        name: String!,
+        email: String!,
+        age: Int
+    }
+
+    type Post {
+        id: ID!,
+        name: String!,
+        published: Boolean!,
     }
 `;
 
+// resolvers
 const resolvers = {
     Query:  {
-        intro: () => 'Hello my self shubham'
+       add : (parent, args, ctx, info) => args.a && args.b ? args.a + args.b : 0,
+       greeting(parent, args, ctx, info) {
+           return args.name ? `Hello ${args.name}` : `Hello`;
+       }, 
+       me() {
+           return {id: '124', name: "shubham", email: 'shubham.chauhan@gmail.com', age: 12}
+       },
+       post() {
+           return {id: 'abc', name: "drama", published: false}
+       }
     }
 };
 
