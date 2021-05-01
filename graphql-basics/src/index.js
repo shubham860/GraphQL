ac95542a5f0581123e5c67b2762b77da40161db9
shubham860 <shubham.chauhan860@gmail.com>
2021-05-02 00:53:40 +0530

    working with scalar and custom  arrays and basics of relatioal data and relational data arrays

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -17,6 +17,26 @@ const users = [{
     name: 'ravero',
     email: 'ravero@gmail.com'
 }
+];
+
+const posts = [{
+    id: '1',
+    title: 'My story',
+    published: true,
+    author: "1"
+},
+{
+    id: '2',
+    title: 'My future',
+    published: false,
+    author: '2'
+},
+{   
+    id: '3',
+    title: 'My past',
+    published: true,
+    author: '3'
+},
 ]
 
 // typeDefs (Schema)
@@ -28,6 +48,7 @@ const typeDefs = `
        greeting(name: String): String!
 
        users(query: [String!]): [User!]!
+       posts(query: [String!]): [Post!]!
        me: User!
        post: Post!
     }
@@ -37,37 +58,41 @@ const typeDefs = `
         name: String!
         email: String!
         age: Int
+        posts: [Post!]! 
     }
 
     type Post {
         id: ID!
-        name: String!
+        title: String!
         published: Boolean!
+        author: User!
     }
 `;
 
 // resolvers
 const resolvers = {
+
     Query:  {
-       add : (parent, args, ctx, info) => args.a && args.b ? args.a + args.b : 0,
 
-       addArray : (parent, args, ctx, info) => {
-         if(args.numbers.length === 0){
-             return 0;
-         }
+    // Beg level resolvers    
+    //    add : (parent, args, ctx, info) => args.a && args.b ? args.a + args.b : 0,
 
-         return args.numbers.reduce((A,C) => A+C)
-       },
+    //    addArray : (parent, args, ctx, info) => {
+    //      if(args.numbers.length === 0){
+    //          return 0;
+    //      }
 
-       grade : () => [1,2,3,4],
+    //      return args.numbers.reduce((A,C) => A+C)
+    //    },
 
-       greeting(parent, args, ctx, info) {
-           return args.name ? `Hello ${args.name}` : `Hello`;
-       }, 
+    //    grade : () => [1,2,3,4],
+
+    //    greeting(parent, args, ctx, info) {
+    //        return args.name ? `Hello ${args.name}` : `Hello`;
+    //    }, 
 
 
 
-  
 
        users: (parent, args, ctx, info) => {
            if(!args.query){
@@ -77,15 +102,40 @@ const resolvers = {
            return users.filter(val => val.name.toLowerCase().includes(args.query[0].toLowerCase()))
        },
 
+
+       posts: (parent, args, ctx, info) => {
+        if(!args.query){
+            return posts
+         }
+         console.log('args',args)
+
+         return posts.filter(val => val.title.toLowerCase().includes(args.query[0].toLowerCase()))
+       },
+
        me() {
            return {id: '124', name: "shubham", email: 'shubham.chauhan@gmail.com', age: 12}
        },
 
 
        post() {
-           return {id: 'abc', name: "drama", published: false}
+           return {id: 'abc', title: "drama", published: false}
        }
+    },
+
+    // relational data of posts of type Post with author
+    Post: {
+     author: (parent, agrs, ctx, info) => {
+         // here parent is the each object of post
+         // (parent.id, parent.title, parent.published, parent.authod) we have access of all val in obj.
+        return   users.find(val => val.id === parent.author)
+     }
+    },
+
+  // relational data of users of type User with posts
+    User: {
+        posts: (parent, args, ctx, info) => posts.filter(post => post.author === parent.id)
     }
+
 };
 
 
